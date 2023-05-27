21f35fe32797e2778a9d9457d3bf3bc2e6c06916
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-05-27 21:24:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+import logging
+import telebot
+
+# Set up logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Telegram Bot Token
+TOKEN = 'YOUR_BOT_TOKEN'
+
+# Private group ID to forward messages
+PRIVATE_GROUP_ID = -123456789  # Replace with the actual private group ID
+
+# Create bot instance
+bot = telebot.TeleBot(TOKEN)
+
+# Handler to forward messages to private group
+@bot.message_handler(func=lambda message: True)
+def forward_message(message):
+    # Forward message to private group
+    bot.forward_message(chat_id=PRIVATE_GROUP_ID,
+                        from_chat_id=message.chat.id,
+                        message_id=message.message_id)
+
+# Handler for /start command
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello! I will forward all messages to the private group.')
+
+# Handler for unknown commands
+@bot.message_handler(func=lambda message: True)
+def unknown(message):
+    bot.reply_to(message, "Sorry, I didn't understand that command.")
+
+# Start the Bot
+bot.polling()
